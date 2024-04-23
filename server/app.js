cd0c435935319014dc2bdbca072669e35c610808
Scott Feichter <scottfeichter@gmail.com>
2024-04-23 07:56:46 -0700

    misc

--- server/app.js
+++ server/app.js
@@ -2,4 +2,10 @@ const express = require(`express`);
 
 const app = express();
 
+app.get(`/`, (req, res) => {
+  res.send(`Hello World`);
+});
 
+const port = 5001;
+
+app.listen(port, console.log(`Server is listening on port ${port}`));
